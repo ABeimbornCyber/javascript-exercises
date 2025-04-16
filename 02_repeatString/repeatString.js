0f98060579fe600c16696330a03af8f13a3030e7
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-16 06:03:09 -0500

    Solved problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    completeString = "";
+    if(num<0){
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        completeString += string;
+    }
+    return completeString;
 };
 
 // Do not edit below this line
