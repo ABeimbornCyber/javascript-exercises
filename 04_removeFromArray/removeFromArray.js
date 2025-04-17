67671c5980fd8492add952bd3b265164a7d3b50d
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-17 09:58:42 -0500

    Completing question 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let returnEmpty = true;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] != args[i]){
+            returnEmpty = false;
+        }
+    }
+    if(returnEmpty){
+        return [];
+    }
+    for(let i = 0; i < arr.length; ++i){
+        for(let j = 0; j < arr.length; j++){
+            if(arr[i] === args[j]){
+                
+                arr.splice(i, 1);
+                if(i != 0){
+                    i--;
+                }
+                
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
